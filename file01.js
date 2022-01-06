69fe4a7e28c1e549912d5a98919a0e431903131b
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-01-07 00:15:40 +0100

    create and copy files

--- file01.js
+++ file01.js
@@ -1,8 +1,18 @@
 let fs = require('fs');
+let path = 'G:/Projects/JavaScript/DevAcademy NodeJS/'
 
-fs.mkdir('NewFolder', CreateDir);
+fs.mkdir('Savegames', CreateDir);
+fs.appendFile('savegame01.txt', 'life-100', createFile);
+fs.rename(path + 'savegame01.txt', path + 'Savegames/savegame01.txt', copySuccess);
 
 function CreateDir() {
     console.log('Ordner erstellt');
 }
 
+function createFile() {
+    console.log('Datei erstellt');
+}
+
+function copySuccess() {
+    console.log('Datei kopiert');
+}
