b93d865063545f0f28fd008156622a32d78191a5
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-01-08 11:37:11 +0100

    file sort 02

--- sort.js
+++ sort.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 let path = require('path');
-processFile('Text.txt');
+processFile('Test.txt');
 
 function processFile(file) {
     let extension = path.extname(file);
     console.log('Extension is: ' + extension);
-    fs.mkdir(extension, CreateDir);
+
+    fs.mkdir(extension, function () {
+        fs.rename(__dirname + '/' + file, __dirname + '/' + extension + '/' + file, copySuccess);
+    });
+
 }
 
-function CreateDir() {
-    console.log('Ordner erstellt');
+function copySuccess() {
+    console.log('Datei kopiert');
 }
