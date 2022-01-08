970b920243c23c7ad597b5678d20d1877c9fb890
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-01-08 11:29:01 +0100

    create extension folder

--- sort.js
+++ sort.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
 let path = require('path');
 processFile('Text.txt');
 
 function processFile(file) {
     let extension = path.extname(file);
     console.log('Extension is: ' + extension);
+    fs.mkdir(extension, CreateDir);
+}
+
+function CreateDir() {
+    console.log('Ordner erstellt');
 }
